ac9974eccd346e2dc40917a3b6817f6b71cc6821
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-10-18 20:45:35 -0300

    Casting

---
+++ src/main/java/Casting.java
@@ -0,0 +1,13 @@
+public class Casting {
+    public static void main(String[] args) {
+
+        // CASTING
+
+        double x = 5.3;
+        int y = (int) x; // explicit conversion (casting) from double to int
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
