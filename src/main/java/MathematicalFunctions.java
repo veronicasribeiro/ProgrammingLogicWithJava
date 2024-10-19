209db035484c9ceed688c4a09c28759125a7a242
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-10-18 21:04:48 -0300

    Mathematical functions: exponentiation, square root, PI and absolute value

---
+++ src/main/java/MathematicalFunctions.java
@@ -0,0 +1,24 @@
+public class MathematicalFunctions {
+    public static void main(String[] args) {
+
+        double x = 2.0;
+        double y = 3.0;
+        double z = -7.0;
+
+        double a = Math.pow(x,y); // exponentiation
+        System.out.println(a);
+
+        double b = Math.sqrt(y); // square root
+        System.out.println(b);
+
+        double c = Math.PI;
+        System.out.println(c);
+
+        double d = Math.abs(z); // absolute value
+        System.out.println(d);
+
+        // It is possible to use functions inside larger expressions
+
+
+    }
+}
