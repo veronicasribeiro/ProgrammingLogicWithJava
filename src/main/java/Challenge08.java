ceac35ab3fd2b40067fbd12a0577cbefd69354e7
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-10-20 18:36:25 -0300

    feat: implemented employee payment calculation
    - Program created to calculate and display the employee's payment based on their hourly rate and hours worked
    - Employee's name, hourly rate, and hours worked are received as input
    - Total payment displayed with an explanatory message

---
+++ src/main/java/Challenge08.java
@@ -0,0 +1,28 @@
+/*
+    Problem: 'Payment'
+    Create a program to read an employee's name, the amount they receive per
+    hour, and the number of hours they worked. At the end, display the
+    employee's payment amount with an explanatory message
+*/
+
+
+import java.util.Scanner;
+
+public class Challenge08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Name: ");
+        String name = scanner.nextLine();
+        System.out.print("Hourly rate: ");
+        double hourlyRate = scanner.nextDouble();
+        System.out.print("Hours worked: ");
+        int hoursWorked = scanner.nextInt();
+
+        double payment = hourlyRate * hoursWorked;
+
+        System.out.println("Payment to " + name + " must be " + payment);
+
+        scanner.close();
+    }
+}
