96898fe8fe8d99f9af0c1213eb57601dcfc2abdf
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-10-20 18:52:48 -0300

    feat: implemented average car consumption calculator
    - Program created to calculate the car's average consumption based on distance traveled and fuel spent
    - Distance and fuel are received as input
    - Average consumption is displayed with three decimal places

---
+++ src/main/java/Challenge09.java
@@ -0,0 +1,30 @@
+/*
+    Problem: 'Consumption'
+    Make a program to read the total distance (in km) traveled by a car, as
+    well as the total fuel consumed by this car when traveling this distance.
+    Your program should show the average consumption of the car, with three
+    decimal places
+*/
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class Challenge09 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("#0.000");
+
+        System.out.print("Distance traveled: ");
+        double distanceTraveled = scanner.nextDouble();
+        System.out.print("Fuel spent: ");
+        double fuelSpent = scanner.nextDouble();
+
+        double averageConsumption = distanceTraveled / fuelSpent;
+
+        String averageConsumptionFormat = df.format(averageConsumption);
+
+        System.out.println("Average consumption = " + averageConsumptionFormat);
+
+        scanner.close();
+    }
+}
