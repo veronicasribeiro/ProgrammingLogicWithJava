7c66c1568a347ed2e401ea7f2369e0198bbfca4c
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-10-31 18:38:33 -0300

    feat: add functionality to determine if a number is even/odd and positive/negative
    - Implemented logic to identify if user-provided numbers are "POSITIVE PAIR", "NEGATIVE PAIR", "NEGATIVE ODD", or "ODD POSITIVE".
    - Added prompt for the number of inputs, iterating with a loop to evaluate each number.
    - Used conditional statements to print the corresponding classification based on each number's parity and sign.

---
+++ src/main/java/com/example/repetitive/structure/ProblemOddEven.java
@@ -0,0 +1,31 @@
+package com.example.repetitive.structure;
+
+import java.util.Scanner;
+
+public class ProblemOddEven {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many numbers are you going to type? ");
+        int n = scanner.nextInt();
+
+        int number;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("Enter a number: ");
+            number = scanner.nextInt();
+
+            if (number % 2 == 0 && number > 0) {
+                System.out.println("POSITIVE PAIR");
+            } else if (number % 2 == 0 && number < 0) {
+                System.out.println("NEGATIVE PAIR");
+            } else if (number % 2 != 0 && number < 0) {
+                System.out.println("NEGATIVE ODD");
+            } else if (number % 2 != 0) {
+                System.out.println("ODD POSITIVE");
+            } else {
+                System.out.println("NULL");
+            }
+        }
+    }
+}
