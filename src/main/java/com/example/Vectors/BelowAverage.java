2925246a20344549ea8895d1f472be2c74e48ae8
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-11-09 20:20:44 -0300

    feat: implemented below-average element filtering in vector
    - Added functionality to input and store values in a vector.
    - Calculated the average value of the elements in the vector.
    - Iterated through the vector to identify and display elements below the average.
    - Ensured resources are properly closed by adding scanner closure at the end.

---
+++ src/main/java/com/example/Vectors/BelowAverage.java
@@ -0,0 +1,35 @@
+package com.example.Vectors;
+
+import java.util.Scanner;
+
+public class BelowAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many elements will the vector have? ");
+        int n = scanner.nextInt();
+
+        double[] array = new double[n];
+        int count = 0;
+        double sumArray = 0;
+
+        for (int i = 0; i < n ; i++) {
+            System.out.print("Enter a number: ");
+            array[i] = scanner.nextDouble();
+            sumArray = sumArray + array[i];
+            count++;
+        }
+
+        double averageArray = sumArray / count;
+
+        System.out.println("\nVECTOR AVERAGE " + averageArray);
+        System.out.println("BELOW AVERAGE ELEMENTS: ");
+        for (int i = 0; i < n ; i++) {
+            if (array[i] < averageArray) {
+                System.out.println(array[i]);
+            }
+        }
+
+        scanner.close();
+    }
+}
