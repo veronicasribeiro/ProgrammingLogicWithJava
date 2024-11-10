982ca62a645558ee9b70b81dd4b8001d06ae9e8a
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-11-10 10:56:34 -0300

    feat: implemented functionality to display students with passing averages
    - Added input for student names and grades.
    - Calculated and stored each student's average in an array.
    - Displayed names of students with an average grade of 6 or higher.
    - Closed scanner resource after completing input operations.

---
+++ src/main/java/com/example/Vectors/Approved.java
@@ -0,0 +1,39 @@
+package com.example.Vectors;
+
+import java.util.Scanner;
+
+public class Approved {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many students will be typed? ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] name = new String[n];
+        double[] averages = new double[n];
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter name, first and second grade of the " + (i + 1) + "st student: ");
+            name[i] = scanner.nextLine();
+
+            double note1 = scanner.nextDouble();
+            double note2 = scanner.nextDouble();
+            scanner.nextLine();
+
+            averages[i] = (note1 + note2) / 2;
+
+        }
+
+        System.out.println("Approved students:");
+
+        for (int i = 0; i < n; i++) {
+            if (averages[i] >= 6) {
+                System.out.println(name[i]);
+            }
+        }
+
+        scanner.close();
+    }
+}
