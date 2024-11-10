6f15baffe028a8f192e3c7dfe1cc27c7ab9943e4
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-11-10 10:16:03 -0300

    feat: implemented functionality to identify oldest person in list
    - Added functionality to input and store names and ages in separate arrays.
    - Iterated through age array to find and track the oldest person's age.
    - Updated oldest person’s name during each comparison to display result accurately.
    - Displayed the name of the oldest person at the end.
    - Closed scanner resource after completing input operations.

---
+++ src/main/java/com/example/Vectors/Older.java
@@ -0,0 +1,37 @@
+package com.example.Vectors;
+
+import java.util.Scanner;
+
+public class Older {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("How many people are you going to type? ");
+        int n = scanner.nextInt();
+        scanner.nextLine();
+
+        String[] name = new String[n];
+        int[] age = new int[n];
+        int older = 0;
+        String olderPerson = "";
+
+        for (int i = 0; i < n; i++) {
+            System.out.println((i + 1) + "st person's details:");
+            System.out.print("Name: ");
+            name[i] = scanner.nextLine();
+
+            System.out.print("Age: ");
+            age[i] = scanner.nextInt();
+            scanner.nextLine();
+
+            if (age[i] > older) {
+                older = age[i];
+                olderPerson = name[i];
+            }
+        }
+
+        System.out.println("OLDER PERSON: " + olderPerson);
+
+        scanner.close();
+    }
+}
