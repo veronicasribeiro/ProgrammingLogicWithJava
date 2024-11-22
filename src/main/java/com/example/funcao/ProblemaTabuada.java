9e9bd2dd41a734189b072ee64e5bf9ce6c95c1a8
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-11-22 19:00:24 -0300

    feat: implementa exibição de tabuada de um número
    - Criado método `mostrarTabuada` para exibir a tabuada de um número fornecido pelo usuário.
    - Adicionado laço de repetição para calcular e imprimir os resultados da multiplicação de 1 a 10.
    - Entrada do número solicitada ao usuário via `Scanner`.

---
+++ src/main/java/com/example/funcao/ProblemaTabuada.java
@@ -0,0 +1,22 @@
+package com.example.funcao;
+
+import java.util.Scanner;
+
+public class ProblemaTabuada {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Voce quer a tabuada de qual numero? ");
+        int n = scanner.nextInt();
+
+        mostrarTabuada(n);
+
+        scanner.close();
+    }
+
+    public static void mostrarTabuada(double n) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(n + " X " + (i + 1) + " = " + (n * (i + 1)));
+        }
+    }
+}
