8998fea1d39cc0a077f9e1b6c9ed253a3bb809f4
veronicasribeiro <veronicaribeiroofficial@gmail.com>
2024-11-23 13:45:31 -0300

    refactor: otimiza a busca pelo menor número utilizando a função 'mostrarMenorDeTres'
    A lógica foi refatorada para encontrar o menor número entre cinco inteiros, utilizando a função 'mostrarMenorDeTres' de forma encadeada, em vez de comparações diretas.

--- src/main/java/com/example/funcao/ProblemaMenorNumero.java
+++ src/main/java/com/example/funcao/ProblemaMenorNumero.java
@@ -6,20 +6,26 @@ public class ProblemaMenorNumero {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite três número inteiros: ");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        System.out.println("Digite cinco número inteiros: ");
+        int n1 = scanner.nextInt();
+        int n2 = scanner.nextInt();
+        int n3 = scanner.nextInt();
+        int n4 = scanner.nextInt();
+        int n5 = scanner.nextInt();
 
-        int menor = mostrarMenorNumero(a, b, c);
 
+        // pensando em outras formas de utilizar a função
+        //int auxiliar = mostrarMenorDeTres(n1, n2, n3);
+        //int menor = mostrarMenorDeTres(auxiliar, n4, n5);
+
+        int menor = mostrarMenorDeTres(mostrarMenorDeTres(n1,n2,n3), n4, n5);
         System.out.println("Menor = " + menor);
 
         scanner.close();
     }
 
     // costumamos colocar nomes diferentes nos parâmetros para enfatizar que pode ser outros nomes (nesse caso foi igual)
-    public static int mostrarMenorNumero(int a, int b, int c) {
+    public static int mostrarMenorDeTres(int a, int b, int c) {
         if (a < b && a < c) {
             return a;
         } else if (b < a && b < c) {
